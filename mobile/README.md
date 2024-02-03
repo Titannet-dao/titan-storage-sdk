b8bb0e9ba0f29af0724272aa7d750ea0101b5122
heroims <heroims@163.com>
2024-02-03 20:07:13 +0800

    support mobile

---
+++ mobile/README.md
@@ -0,0 +1,13 @@
+# Build Mobile SDK
+
+## iOS
+```
+cd /titan-storage-sdk/mobile
+gomobile bind -target=ios
+```
+
+## Andoird
+```
+cd /titan-storage-sdk/mobile
+gomobile bind -target=android
+```
---
+++ mobile/main.go
@@ -0,0 +1,99 @@
+package TitanStorageMobile
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	storage "github.com/Filecoin-Titan/titan-storage-sdk"
+)
+
+type ProgressHandler interface {
+	OnProgress(doneSize int64, totalSize int64)
+}
+
+var storage_api storage.Storage
+
+func StorageInit(titanURL string, apiKey string, groupid int) (err error) {
+	storage_api, err = storage.NewStorage(&storage.Config{TitanURL: titanURL, APIKey: apiKey, GroupID: groupid})
+	return
+}
+
+func CreateGroup(name string, parentID int) error {
+	return storage_api.CreateGroup(context.Background(), name, parentID)
+}
+
+func ListGroups(parentID, limit, offset int) (resp string, err error) {
+	originResp, err1 := storage_api.ListGroups(context.Background(), parentID, limit, offset)
+	if err1 != nil {
+		err = err1
+		return
+	}
+	jsonData, err2 := json.Marshal(originResp)
+	if err2 != nil {
+		err = err2
+		return
+	}
+	resp = string(jsonData)
+	return
+}
+
+func DeleteGroup(groupID int) error {
+	return storage_api.DeleteGroup(context.Background(), groupID)
+}
+
+func ListUserAssets(limit, offset int) (resp string, err error) {
+	originResp, err1 := storage_api.ListUserAssets(context.Background(), limit, offset)
+	if err1 != nil {
+		err = err1
+		return
+	}
+	jsonData, err2 := json.Marshal(originResp)
+	if err2 != nil {
+		err = err2
+		return
+	}
+	resp = string(jsonData)
+	return
+}
+
+func UploadFilesWithPath(filePath string, handler ProgressHandler) (string, error) {
+	progress := func(doneSize int64, totalSize int64) {
+		if handler != nil {
+			handler.OnProgress(doneSize, totalSize)
+		}
+	}
+	originCid, err := storage_api.UploadFilesWithPath(context.Background(), filePath, progress)
+	return originCid.String(), err
+}
+
+func UploadFileWithURL(url string, handler ProgressHandler) (string, error) {
+	progress := func(doneSize int64, totalSize int64) {
+		if handler != nil {
+			handler.OnProgress(doneSize, totalSize)
+		}
+	}
+	cid, url, err := storage_api.UploadFileWithURL(context.Background(), url, progress)
+
+	return fmt.Sprintf("{\"rootCID\":\"%s\", \"url\":\"%s\"}", cid, url), err
+}
+
+func GetURL(rootCID string) (string, error) {
+	return storage_api.GetURL(context.Background(), rootCID)
+}
+
+func GetFileWithCid(url string) (data []byte, err error) {
+	readerCloser, err1 := storage_api.GetFileWithCid(context.Background(), url)
+	if err1 != nil {
+		err = err1
+		return
+	}
+	data, err2 := io.ReadAll(readerCloser)
+	err = err2
+	return
+}
+
+func Delete(rootCID string) error {
+	return storage_api.Delete(context.Background(), rootCID)
+}
